f58bd17d0c04e2e35e182710fc7daa6470226261
Andrew Zahalski <info@zahalski.dev>
2022-07-02 13:46:44 +0300

    fix updater.php

--- update/1.0.11/updater.php
+++ update/1.0.11/updater.php
@@ -1,5 +1,19 @@
 <?
 $moduleId = "awz.ydelivery";
+
 $connection = \Bitrix\Main\Application::getConnection();
-$sql = 'ALTER TABLE `b_awz_ydelivery_offer` IF EXISTS `LAST_STATUS` ADD `LAST_STATUS` varchar(65) DEFAULT NULL;';
-$connection->queryExecute($sql);
+$checkColumn = false;
+$checkTable = false;
+$recordsRes = $connection->query("select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='b_awz_ydelivery_offer'");
+while($dt = $recordsRes->fetch()){
+	$checkTable = true;
+	if($dt['COLUMN_NAME'] == 'LAST_STATUS'){
+		$checkColumn = true;
+		break;
+	}
+
+}
+if($checkTable && !$checkColumn){
+	$sql = 'ALTER TABLE `b_awz_ydelivery_offer` ADD `LAST_STATUS` varchar(65) DEFAULT NULL';
+	$connection->queryExecute($sql);
+}
