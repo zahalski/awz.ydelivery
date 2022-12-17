11734da4e9873c1a20e834e0ba13bb769a7b710c
Andrew Zahalski <info@zahalski.dev>
2022-12-18 00:35:43 +0300

    change history version generator

--- build/cl.py
+++ build/cl.py
@@ -1,16 +1,37 @@
 import os
 from tools import *
 
-change_log = []
-change_log.append('## История версий')
+change_log = {}
+ver = '1.0.0'
+ver_list = [f'{int(x):04}' for x in ver.split(".")]
+ver_key_str = '.'.join(ver_list)
+change_log[ver_key_str] = []
+change_log[ver_key_str].append('## История версий')
 
 updates_path = os.path.abspath('../update/')
 readme_file = os.path.abspath('../README.md')
 for name in os.listdir(updates_path):
     with open(os.path.join(updates_path, name, 'description.ru'), 'r', encoding='utf-8') as fr:
-        change_log.append('*v'+str(name)+'*    ')
+        ver = str(name)
+        ver_list = [f'{int(x):04}' for x in ver.split(".")]
+        ver_key_str = '.'.join(ver_list)
+        change_log[ver_key_str] = []
+        change_log[ver_key_str].append('')
+        change_log[ver_key_str].append('**version '+ver+'**    ')
         for line in fr:
-            change_log.append(line.strip()+'    ')
+            line_formated = line.strip()
+            if line_formated[0] == '-' and line_formated[1] != ' ':
+                line_formated = '- '+line_formated[1:]
+            if line_formated[-1] == '.':
+                line_formated = line_formated[:-1] + ';'
+            elif line_formated[-1] != ';':
+                line_formated = line_formated+';'
+            change_log[ver_key_str].append(line_formated+'    ')
+        if change_log[ver_key_str][-1][-5] == ';':
+            change_log[ver_key_str][-1] = change_log[ver_key_str][-1][:-5]+'.    '
+
+
+sorted_change_log = dict(sorted(change_log.items()))
 
 all_rows = []
 find_cl = False
@@ -22,8 +43,10 @@ with open(readme_file, 'r', encoding='utf-8') as fr:
                 find_cl = True
         else:
             if '<!-- cl-end -->' in line:
-                for ln in change_log:
-                    all_rows.append(ln+"\n")
+                for i in sorted_change_log:
+                    lns = sorted_change_log[i]
+                    for ln in lns:
+                        all_rows.append(ln+"\n")
                 all_rows.append(line)
                 find_cl = False
 
