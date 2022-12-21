b81ef9e25f2db663621f9b338151b54f5c73ed23
Andrew Zahalski <info@zahalski.dev>
2022-12-22 01:34:23 +0300

    запрет создания обновления поверх

--- build/checkup.py
+++ build/checkup.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("../")
 from build.tools import *
 
@@ -6,20 +7,40 @@ conf = get_config()
 updates_path = os.path.abspath(conf['updates_path'])
 module_path = os.path.abspath(conf['module_path'])
 
-files = get_changed(updates_path, '1.0.30')
+change_log = {}
+for name in os.listdir(updates_path):
+    if name[-5:] == '.json':
+        continue
+    else:
+        ver = str(name)
+        ver_list = [f'{int(x):04}' for x in ver.split(".")]
+        ver_key_str = '.'.join(ver_list)
+        change_log[ver_key_str] = []
+
+sorted_change_log = dict(sorted(change_log.items()))
+last_version_ar = '.'.join([str(int(_)) for _ in sorted_change_log.popitem()[0].split('.')])
+last_version_updated = '.'.join([str(int(_)) for _ in sorted_change_log.popitem()[0].split('.')])
+
+args = sys.argv
+if len(args) > 1:
+    last_version_updated = args[1]
+
+files = get_changed(updates_path, last_version_updated)
 last_version = get_module_version(module_path)
 
-#добавление папки с обновлением
+# добавление папки с обновлением
 new_version_path = os.path.abspath(os.path.join(updates_path, last_version))
 if not os.path.isdir(new_version_path):
     os.makedirs(new_version_path)
     print("created path", new_version_path)
+else:
+    raise Exception(new_version_path + ' is exists')
 
-#добавление файла с описанием обновления
+# добавление файла с описанием обновления
 new_version_desc = os.path.join(new_version_path, 'description.ru')
 if not os.path.isfile(new_version_desc):
     with open(new_version_desc, "w", encoding='utf-8') as outfile:
-        outfile.write("- обновление "+last_version)
+        outfile.write("- обновление " + last_version)
         print("created file", "description.ru")
 
 # сколько папок в пути к модулю
@@ -40,7 +61,7 @@ for file_updated_path in files:
         temp_path = new_version_path
         for dir in file_copy_path:
             temp_path = os.path.join(temp_path, dir)
-            #проверка на конец пути (папка с именем файла не требуется)
+            # проверка на конец пути (папка с именем файла не требуется)
             if os.path.isfile(file_path) and (file_splited_path[-1] == temp_path[-len(file_splited_path[-1]):]):
                 continue
             if not os.path.isfile(temp_path) and not os.path.isdir(temp_path):
