6a03d6565bea7d9f6359e5076347ddefed20e5d2
Andrew Zahalski <info@zahalski.dev>
2022-12-23 20:10:33 +0300

    update build, move to 1.0.35

---
+++ update/1.0.35/lang/ru/js/js_script.php
@@ -0,0 +1,12 @@
+<?php
+$MESS["AWZ_YDELIVERY_JS_CLOSE"] = "закрыть";
+$MESS["AWZ_YDELIVERY_JS_CHOISE"] = "выбрать";
+$MESS["AWZ_YDELIVERY_JS_SERV_ERR"] = "Ошибка при получении данных.";
+$MESS["AWZ_YDELIVERY_JS_SERV_LOADER"] = "Загрузка данных...";
+$MESS["AWZ_YDELIVERY_JS_SERV_PAY_CASH"] = "наличные";
+$MESS["AWZ_YDELIVERY_JS_SERV_PAY_CARD"] = "карта";
+$MESS["AWZ_YDELIVERY_JS_SERV_PAY_PREPAY"] = "предоплата";
+$MESS["AWZ_YDELIVERY_JS_SERV_TYPE_PVZ"] = "ПВЗ";
+$MESS["AWZ_YDELIVERY_JS_SERV_TYPE_TERMINAL"] = "постамат";
+$MESS["AWZ_YDELIVERY_JS_ADDRESS"] = "Доставить сюда!";
+$MESS["AWZ_YDELIVERY_JS_ADDRESS_GEO_ERR"] = "Извините, геокодер недоступен в данный момент.";
---
+++ update/1.0.35/lang/ru/lib/api/controller/standart.php
@@ -0,0 +1,3 @@
+<?php
+$MESS['AWZ_YDELIVERY_API_CONTROL_STANDART_PROFILE_ERR'] = 'Идентификатор доставки не найден';
+$MESS['AWZ_YDELIVERY_API_CONTROL_STANDART_ADDRGEO_ERR'] = 'Адрес или geo_id обязателен';
